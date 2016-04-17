e1359b7c7db07c490b08c4f235aeed55457d81c6
destinityx2 <ivan.afonichkin@gmail.com>
2016-04-17 22:02:41 +0300

    SQL changes

--- Teammer/settings.py
+++ Teammer/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'teammer',
         'USER': 'admin',
-        'PASSWORD': 'qwer',
+        'PASSWORD': 'qwerqwe@',
         'HOST': 'localhost',
         'PORT': '',
     }
